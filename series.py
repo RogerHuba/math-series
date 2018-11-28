29208ebd7172a2ab3676f6177ab8d6e90035b306
Roger Huba <rogerhuba@gmail.com>
2018-11-27 23:25:52 -0800

    file upsated and added testing

--- series.py
+++ series.py
@@ -2,6 +2,6 @@ def fibonacci(n):
     if(n <= 1):
         return n
     else:
-        return (fib(n-1) + fib(n-2))
+        return ((n-1) + (n-2))
 
 # if __name__ == "__main__":
--- test_series.py
+++ test_series.py
@@ -1,8 +1,9 @@
 from series import fibonacci
+import pytest
 
 def test_single_number():
     """test function which tests by bringing in an even numbered list
     """
-    actual = 1
-    expected = [1]
+    actual = 5
+    expected = 7
     assert fibonacci(actual) == expected
