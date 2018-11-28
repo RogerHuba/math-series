47ad19d8fc0d4698ddedf0e202343392f3684aa3
Roger Huba <rogerhuba@gmail.com>
2018-11-28 08:21:17 -0800

    fix sequence

--- test_series.py
+++ test_series.py
@@ -1,9 +1,8 @@
 from series import fibonacci
-import pytest
 
 def test_single_number():
     """test function which tests by bringing in an even numbered list
     """
-    actual = 6
-    expected = 8
+    actual = 4
+    expected = 3
     assert fibonacci(actual) == expected
