0f44c3a5f467de8c1a143fadcdf13281324e19e1
Roger Huba <rogerhuba@gmail.com>
2018-11-28 08:38:47 -0800

    additions to lucas and testing

--- series.py
+++ series.py
@@ -3,8 +3,26 @@ def fibonacci(n):
     number2 = int(1)
     count = int(0)
     while count < n:
+        if n == 0:
+            return 0
         nth = number1 + number2
         number1 = number2
         number2 = nth
         count += 1
     return nth
+
+
+def lucas(n):
+    number1 = int(1)
+    number2 = int(1)
+    count = int(0)
+    if n == 0:
+        return 0
+    while count < n:
+        nth = number1 + number2
+        number1 = number2
+        number2 = nth
+        count += 1
+    return nth
+
+# if __name__ == "__main__"
--- test_series.py
+++ test_series.py
@@ -1,8 +1,41 @@
 from series import fibonacci
+from series import lucas
 
-def test_single_number():
-    """test function which tests by bringing in an even numbered list
+def test_single_number_fib():
+    """test fib function which tests by bringing in a single number
     """
     actual = 4
     expected = 3
     assert fibonacci(actual) == expected
+
+
+def test_zero_number_fib():
+    """test fib function which tests by bringing in a zero number
+    """
+    actual = 0
+    expected = 0
+    assert fibonacci(actual) == expected
+
+
+def test_double_number_fib():
+    """test fib function which tests by bringing in a double digit number
+    """
+    actual = 10
+    expected = 55
+    assert fibonacci(actual) == expected
+
+
+def test_single_number_lucas():
+    """test lucas function which tests by bringing in a single number
+    """
+    actual = 4
+    expected = 5
+    assert lucas(actual) == expected
+
+
+def test_zero_number_lucas():
+    """test lucas function which tests by bringing in a zero number
+    """
+    actual = 0
+    expected = 1
+    assert lucas(actual) == expected
