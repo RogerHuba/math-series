26f47aab8f870faedbbfec0ef07e3701987e8c02
Roger Huba <rogerhuba@gmail.com>
2018-12-03 08:44:35 -0800

    re-submit

--- series.py
+++ series.py
@@ -1,10 +1,47 @@
+import sys
+
+
 def fibonacci(n):
-    number1 = int(0)
-    number2 = int(1)
-    count = int(0)
-    while count < n:
+    """Generate the nth number of the fib sequence"""
+    number1 = 0
+    number2 = 1
+    count = 2
+
+    if type(n) is not int or n < 0:
+        raise TypeError('Please enter a positive number')
+
+    if n == 0 or n == 1:
+        return n
+    while count < n + 1:
         nth = number1 + number2
         number1 = number2
         number2 = nth
         count += 1
     return nth
+
+
+def lucas(n):
+    """Generate the nth instance of the lucas
+    """
+    number1 = 1
+    number2 = 2
+    nth = 1
+
+    if type(n) is not int:
+        raise TypeError('Please enter a number')
+
+    if n == 0:
+        return 2
+    if n == 1:
+        return 1
+    for count in range(2, n + 1):
+        nth = number1 + number2
+        number2 = number1
+        number1 = nth
+    return nth
+
+
+if __name__ == "__main__":
+    print(fibonacci(4))
+    print (lucas(4))
+    sys.exit()
--- test_series.py
+++ test_series.py
@@ -1,8 +1,50 @@
 from series import fibonacci
+from series import lucas
 
-def test_single_number():
-    """test function which tests by bringing in an even numbered list
+def test_single_number_fib():
+    """test fibonacci function by bringing in a single number
     """
     actual = 4
     expected = 3
     assert fibonacci(actual) == expected
+
+
+def test_zero_number_fib():
+    """test fibonacci function by bringing in a zero number
+    """
+    actual = 0
+    expected = 0
+    assert fibonacci(actual) == expected
+
+
+def test_double_number_fib():
+    """test fibonacci function by bringing in a double digit number
+    """
+    actual = 10
+    expected = 55
+    assert fibonacci(actual) == expected
+
+
+def test_single_number_lucas():
+    """test lucas function by bringing in a single number
+    """
+    actual = 4
+    expected = 7
+    assert lucas(actual) == expected
+
+
+def test_single_double_lucas():
+    """test lucas function by bringing in a double number
+    """
+    actual = 10
+    expected = 123
+    assert lucas(actual) == expected
+
+
+def test_zero_number_lucas():
+    """test lucas function by bringing in a zero number
+    """
+    actual = 0
+    expected = 2
+    assert lucas(actual) == expected
+
